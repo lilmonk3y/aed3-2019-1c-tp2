32b151664d50a9d85c2da28a4216949e4ae6d2a1
lilmonk3y <christiannahuelrivera@gmail.com>
2019-05-21 01:01:51 -0300

    mst full tested

--- project/test/utils/GetMSTTest.cpp
+++ project/test/utils/GetMSTTest.cpp
@@ -50,7 +50,6 @@ struct MSTTest : testing::Test {
 
     ~MSTTest(){
         delete getMST;
-        delete graph;
         delete graph1;
         delete graph2;
     }
@@ -66,6 +65,7 @@ TEST_F(MSTTest,whenGetPrimMSTOfACircuit_mustReturnAnMSTOfTheOriginalGraph){
     Graph *actualMST = getMST->getMST(graph);
 
     ASSERT_EQ(10 , actualMST->getTotalCost());
+    delete graph;
 }
 
 TEST_F(MSTTest,whenGetKruskalCompressedMSTOfATree_mustReturnTheSameGraph){
@@ -75,6 +75,7 @@ TEST_F(MSTTest,whenGetKruskalCompressedMSTOfATree_mustReturnTheSameGraph){
     Graph *actualMST = getMST->getMST(graph);
 
     ASSERT_EQ(5,actualMST->getTotalCost());
+    delete graph;
 }
 
 TEST_F(MSTTest, whenGraph1PrimMST_shouldHaveTotalWeight6)
@@ -91,4 +92,63 @@ TEST_F(MSTTest, whenGraph2PrimMST_shouldHaveTotalWeight37)
 
     float actualWeight = mst->getTotalCost();
     ASSERT_EQ(37, actualWeight);
+}
+
+TEST_F(MSTTest, testing1){
+    graph = new AdjacencyListGraph(4);
+    graph->addEdge(0,1,10);
+    graph->addEdge(1,2,10);
+    graph->addEdge(2,3,10);
+    graph->addEdge(3,0,10);
+    graph->addEdge(0,2,1);
+    graph->addEdge(1,3,1);
+
+    auto mst = getMST->getMST(graph);
+    float actualWeigth = mst->getTotalCost();
+
+    ASSERT_EQ(12, actualWeigth);
+    delete graph;
+}
+
+TEST_F(MSTTest,testing2){
+    graph = new AdjacencyListGraph(7);
+    graph->addEdge(0,2,1);
+    graph->addEdge(0,1,5);
+    graph->addEdge(0,3,4);
+    graph->addEdge(2,3,3);
+    graph->addEdge(3,5,8);
+    graph->addEdge(5,4,7);
+    graph->addEdge(4,6,9);
+    graph->addEdge(1,5,6);
+    graph->addEdge(2,4,2);
+
+    auto mst = getMST->getMST(graph);
+    float actualWeigth = mst->getTotalCost();
+
+    ASSERT_EQ(26, actualWeigth);
+    delete graph;
+}
+
+TEST_F(MSTTest, testing3){
+    graph = new AdjacencyListGraph(8);
+    graph->addEdge(0,1,11);
+    graph->addEdge(0,2,9);
+    graph->addEdge(0,3,6);
+    graph->addEdge(1,3,5);
+    graph->addEdge(1,4,7);
+    graph->addEdge(2,3,12);
+    graph->addEdge(2,5,6);
+    graph->addEdge(3,5,3);
+    graph->addEdge(3,4,4);
+    graph->addEdge(3,6,7);
+    graph->addEdge(4,6,2);
+    graph->addEdge(5,6,8);
+    graph->addEdge(5,7,10);
+    graph->addEdge(6,7,6);
+
+    auto mst = getMST->getMST(graph);
+    float actualWeigth = mst->getTotalCost();
+
+    ASSERT_EQ(32, actualWeigth);
+    delete graph;
 }

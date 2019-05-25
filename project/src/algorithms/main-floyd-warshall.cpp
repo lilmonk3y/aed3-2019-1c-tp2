b1e1d998cbdf867ab7fa886a8a6ba8d79ee654b2
Luis Greco <lgreco@dc.uba.ar>
2019-05-24 22:10:28 -0300

    Avances en Floyd Warshall

--- project/src/algorithms/main-floyd-warshall.cpp
+++ project/src/algorithms/main-floyd-warshall.cpp
@@ -11,14 +11,26 @@ int main() {//Todo: Cambiar el main de bellman para evitar main duplicado
   GasGraph graph(cin);
 
   // Cálculo de caminos mínimos e impresión de la salida
-  vector< vector<ulong> > D(graph.getVertices(), vector<ulong>(graph.getVertices(), infty));  //Matriz de pesos
-  floydWarshall(graph, D);
-  for(uint i = 0; i < D.size(); ++i){
-  	for(uint j = 0; j < D[i].size(); ++j){
+  vector< vector<ulong> > D;  //Matriz de pesos
+  vector< vector<ulong> > R; //Matríz de resultado
+  floydWarshall(graph, D, R);
+
+  for(uint i = 0; i < R.size(); ++i){
+  	for(uint j = 0; j < R[i].size(); ++j){
   		if(i == j) continue;
-  		cout << D[i][j] << " ";
+  		cout << i << " " << j << " " << R[i][j] << " ";
   		cout << endl;
   	}
   }
+
+/*  cout << "Todos los caminos: " << endl;
+  for(uint i = 0; i < D.size(); ++i){
+  	for(uint j = 0; j < D[i].size(); ++j){
+  		if(i == j) continue;
+  		cout << i << " " << j << " " << D[i][j] << " ";
+  		cout << endl;
+  	}
+  }
+*/
   return 0;
 }

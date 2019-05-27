bf0355f84ab6e91dc0be899ceba1856d11fdb884
Axel Arrondo <aarrondo@ar0fvfxj15bhv2j.ml.com>
2019-05-26 22:43:00 -0300

    nuevo test

--- project/test/parteUno/SegmentationTest.cpp
+++ project/test/parteUno/SegmentationTest.cpp
@@ -13,7 +13,8 @@ struct SegmentationAlgorithmTest : testing::Test{
 
     SegmentationAlgorithmTest(){
         AdjacencyListGraph* grafo =  new AdjacencyListGraph(1);
-        segmentationAlgorithm = new SegmentationAlgorithm(grafo);
+        int segmentationScale = 2;
+        segmentationAlgorithm = new SegmentationAlgorithm(grafo,segmentationScale);
     }
 
     ~SegmentationAlgorithmTest(){
@@ -24,12 +25,56 @@ struct SegmentationAlgorithmTest : testing::Test{
 
 TEST_F(SegmentationAlgorithmTest, simpleSegmentationWorks){
     AdjacencyListGraph* grafo =  new AdjacencyListGraph(4);
-    grafo->addEdge(0, 1, 2);
-    grafo->addEdge(1, 2, 180);
-    grafo->addEdge(2, 3, 160);
-    grafo->addEdge(3, 1, 200);
+    //0:
+    grafo->addEdge(0, 1, 174);
+    grafo->addEdge(0, 3, 5);
+    grafo->addEdge(0, 4, 189);
 
-    segmentationAlgorithm = new SegmentationAlgorithm(grafo);
+    //1:
+    grafo->addEdge(1, 2, 4 );
+    grafo->addEdge(1, 4, 220);
+    grafo->addEdge(1, 5, 150);
+
+    //2:
+    grafo->addEdge(2, 5, 100 );
+    grafo->addEdge(2, 4, 170 );
+
+    //3:
+    grafo->addEdge(3, 4, 210 );
+    grafo->addEdge(3, 6, 195 );
+    grafo->addEdge(3, 1, 199 );
+    grafo->addEdge(3, 7, 159 );
+
+    //5:
+    grafo->addEdge(5,4 ,200  );
+    grafo->addEdge(5,8 , 4 );
+
+    //6:
+    grafo->addEdge(6,4 , 4  );
+    grafo->addEdge(6,7 , 2 );
+
+    //7:
+    grafo->addEdge(7,4 , 0  );
+    grafo->addEdge(7,8 , 180  );
+    grafo->addEdge(7,5 , 177  );
+
+    //8:
+    grafo->addEdge(8,4 , 160);
+
+    int segmentationScale = 2; // con 1 falla,con 100 anda bien, con 1000 falla
+    segmentationAlgorithm = new SegmentationAlgorithm(grafo,segmentationScale);
+    DisjoinSet* disjointSet = segmentationAlgorithm->graphSementationIntoSets();
+
+    ASSERT_TRUE(disjointSet->find(0) == disjointSet->find(1) );
+    ASSERT_TRUE(disjointSet->find(1) != disjointSet->find(2) &&  disjointSet->find(2) != disjointSet->find(3));
+}
+
+TEST_F(SegmentationAlgorithmTest, image3x3pixelsWith3Areas){
+    AdjacencyListGraph* grafo =  new AdjacencyListGraph(9);
+    grafo->addEdge(0, 1, 2);
+
+    int segmentationScale = 2; // con 1 falla,con 100 anda bien, con 1000 falla
+    segmentationAlgorithm = new SegmentationAlgorithm(grafo,segmentationScale);
     DisjoinSet* disjointSet = segmentationAlgorithm->graphSementationIntoSets();
 
     ASSERT_TRUE(disjointSet->find(0) == disjointSet->find(1) );
